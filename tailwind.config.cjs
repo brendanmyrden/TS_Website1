d12f8471bea07e83bd3099c1ccf6a8b8bcacc81f
Brendan Myrden <myrdenbrendan@proton.me>
2024-06-20 00:29:40 -0400

    .cjs addition

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
   theme: {
     extend: {
       colors : {
-      },
+      "gray-20": "#F8F4EB",
+      "gray-50": "#EFE6E6",
+      "gray-100": "#DFCCCC",
+      "gray-500": "#5E0000",
+      "primary-100": "#FFE1E0",
+      "primary-100": "#FFA6E0",
+      "primary-100": "#FF6B66",
+      "secondary-400"
+    }
     },
   },
 
