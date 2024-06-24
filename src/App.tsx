31a631bd029f989b84a791cdd2f62210d3ddbb82
Brendan Myrden <myrdenbrendan@proton.me>
2024-06-24 01:16:29 -0400

    Index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import Navbar from "@/scenes/navbar";
+
 function App() {
 
-  return <div className="app">app</div>;
+  return <div className="app bg-gray-20 bg">app</div>;
 }
 
 export default App;
